5afa339ffe08870563e00e13e7f7fa1ee3e99749
Wesley Bliss <wesley.bliss@gmail.com>
2015-01-30 19:04:21 -0500

    Ok, it seems location is accurate - it's the weather service that's fucking up.

--- src/client/js/example.js
+++ src/client/js/example.js
@@ -22,7 +22,10 @@ window.onload = function() {
             alert( arg.error );
             return false;
         }
-        //alert( arg.list[0].weather[0].description );
+        $('#forecast-coords').html(
+            '<b>Lat:</b> ' + arg.city.coord.lat + '<br/>' +
+            '<b>Lng:</b> ' + arg.city.coord.lon
+        );
         $('#forecast-general').html(
             arg.list[0].weather[0].description +
             ' in ' + arg.city.name
@@ -31,6 +34,7 @@ window.onload = function() {
     
     var button = document.querySelector('#get-forecast');
     button.onclick = function(e) {
+        $('#status').html( 'Getting your location...' );
         getLocation( function( err, pos ) {
             ipc.send( 'asynchronous-message', {
                 action: 'get-forecast',
@@ -52,10 +56,31 @@ window.onload = function() {
  * @param  {Function} done Callback( err, {lat:x, lon:y})
  */
 var getLocation = function( done ) {
-    navigator.geolocation.getCurrentPosition( function( pos ) {
+    
+    // Due to a bug in Atom, we'll have to do this manually for now.
+    /*navigator.geolocation.getCurrentPosition( function( pos ) {
         done( false, {
             lat: pos.coords.latitude,
             lng: pos.coords.longitude
         });
+    });*/
+    
+    var manualGeoLoc = 'https://maps.googleapis.com/maps/api/browserlocation/json?browser=chromium&sensor=true';
+    $.getJSON( manualGeoLoc, function( res ) {
+        done( false, {
+            lat: res.location.lat,
+            lng: res.location.lng
+        });
     });
+    
+    /* Example response:
+    {
+        accuracy: 477,
+        location: {
+            lat: 40.7229971,
+            lng: -74.00062129999999
+        },
+        status: "OK"
+    }*/
+    
 };
